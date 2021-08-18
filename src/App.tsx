7074f4d41ecf4eebbcea8981b7d3542752d2818f
Laiane Suzart <laiane@pm.me>
2021-08-18 15:10:32 -0300

    style: header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import { Header } from 'components/Header'
 import 'styles/global.scss'
 
 function App() {
   return (
-    <div>
-    </div>
+    <Header theme="light"/>
   );
 }
 
